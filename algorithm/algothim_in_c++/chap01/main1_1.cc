a4e7bdcb573209896a5d0f54e9f26c13f13dc793
sunqiankun <sunqiankun@le.com>
2016-10-28 19:21:25 +0800

    alogorithm c++ chap01

---
+++ algorithm/algothim_in_c++/chap01/main1_1.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+static const int N = 10000;
+
+int main() {
+  int i, p, q, id[N];
+  for (i = 0; i < N; ++i) {
+    id[i] = i;
+  }
+  while (cin >> p >> q) {
+    int t = id[p];
+    if (t == id[q]) continue;
+    for (i = 0; i < N; ++i) {
+      if (id[i] == t) {
+        id[i] = id[q];
+      }
+    }
+    cout << " " << p << " " << q << endl;
+  }
+  for (i = 0; i < 10; ++i) {
+    cout << id[i] << " ";
+  }
+  getchar();
+  return 0;
+}
---
+++ algorithm/algothim_in_c++/chap01/main2.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+static const int N = 10000;
+
+int main() {
+  int i, j, p, q, id[N];
+  for (i = 0; i < N; ++i) {
+    id[i] = i;
+  }
+  while (cin >> p >> q) {
+    for (i = p; i != id[i]; i = id[i]);
+    for (j = q; j != id[j]; j = id[j]);
+    if (i == j) continue;
+    id[i] = j;
+    cout << " " << p << " " << q << endl;
+  }
+  for (i = 0; i < 10; ++i) {
+    cout << id[i] << " ";
+  }
+  getchar();
+  return 0;
+}
