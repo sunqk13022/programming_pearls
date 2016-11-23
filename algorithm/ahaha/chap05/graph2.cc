01fd64b276251fb7296f7e7f3bfee231655b28e0
sunqiankun <sunqiankun@le.com>
2016-11-23 19:18:45 +0800

    graph

---
+++ algorithm/ahaha/chap05/graph2.cc
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main() {
+  int i, j, n, m, a, b, cur, book[101] = {0}, e[101][101];
+  int que[10001], head, tail;
+  scanf("%d %d", &n, &m);
+
+  for (i = 1; i <= n; ++i) {
+    for (j = 1; j <=n; ++j) {
+      if (i == j) e[i][j] = 0;
+      else e[i][j] = 99999999;
+    }
+  }
+
+  for (i = 1; i <= m; ++i) {
+    scanf("%d %d", &a, &b);
+    e[a][b] = 1;
+    e[b][a] = 1;
+  }
+
+  head = 1;
+  tail = 1;
+
+  que[tail] = 1;
+  ++tail;
+  book[1] = 1;
+  while (head < tail) {
+    cur = que[head];
+    for (i = 1; i <= n; ++i) {
+      if (e[cur][i] == 1 && book[i] == 0) {
+        book[i] = 1;
+        que[tail] = i;
+        ++tail;
+      }
+      if (tail > n) {
+        break;
+      }
+    }
+    ++head;
+  }
+
+  for (i = 1; i < tail; ++i) {
+    printf("%d ", que[i]);
+  }
+  getchar();
+  return 0;
+}
---
+++ algorithm/ahaha/chap05/run1.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+# coding=utf-8
+
+import os
+
+os.system("./graph1 < data1")
